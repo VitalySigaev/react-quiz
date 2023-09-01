ace89010a823b3715cf21c00c1d49901f4644157
VitalyFront <vitalyasigaev@gmail.com>
2023-09-01 17:02:58 +0300

    second-commit

--- OneDrive/Desktop/React/react-quiz/src/components/Result/Result.jsx
+++ OneDrive/Desktop/React/react-quiz/src/components/Result/Result.jsx
@@ -4,7 +4,7 @@ function Result({ correct, questions }) {
         <div className="result">
             <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
             <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-            <a href='/'> <button>Попробовать снова</button></a>
+            <button onClick={() => window.location.reload()}>Попробовать снова</button>
         </div>
     );
 }

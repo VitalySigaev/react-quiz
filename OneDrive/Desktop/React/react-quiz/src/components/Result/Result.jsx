b20deaa41ef0912766ddf4a5f65825e8c8bcdfc5
VitalyFront <vitalyasigaev@gmail.com>
2023-09-01 19:40:02 +0300

    fix

--- OneDrive/Desktop/React/react-quiz/src/components/Result/Result.jsx
+++ OneDrive/Desktop/React/react-quiz/src/components/Result/Result.jsx
@@ -4,7 +4,7 @@ function Result({ correct, questions }) {
         <div className="result">
             <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
             <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
-            <button onClick={() => window.location.reload()}>Попробовать снова</button>
+            <button onClick={() => window.location.()}>Попробовать снова</button>
         </div>
     );
 }
